7b4b9ac94af1991e81274fbdd437683e3911b7cf
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-01-09 13:11:08 -0300

    intento cors con @bean webmvcConfigurer

--- src/main/java/com/compasso/CompassoApplication.java
+++ src/main/java/com/compasso/CompassoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -19,6 +20,17 @@ public class CompassoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CompassoApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://compasso-front.web.app/");
+			}
+		};
+	}
+
 
 	@Bean
 	public CommandLineRunner init() {
