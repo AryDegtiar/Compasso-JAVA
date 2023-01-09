64d0b24784b14ad1338f4fcbb9f125b68118d8bc
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-01-09 12:08:00 -0300

    se creo cors origin con link de el front

--- src/main/java/com/compasso/controlador/ControladorProducto.java
+++ src/main/java/com/compasso/controlador/ControladorProducto.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "https://compasso-front.web.app/")
 @RequestMapping("/productos")
 public class ControladorProducto {
     @Autowired
